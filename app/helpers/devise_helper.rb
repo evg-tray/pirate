281cfdfec7a5f19b1c9d0a812a9c3c5037753a7d
Evg <evg.tray@gmail.com>
2017-06-20 12:15:38 +0300

    Edit recipes views

--- app/helpers/devise_helper.rb
+++ app/helpers/devise_helper.rb
@@ -1,15 +1,5 @@
 module DeviseHelper
   def devise_error_messages!
-    return "" if resource.errors.empty?
-
-    messages = resource.errors.full_messages.map{ |msg| "<p>#{msg}.</p>"}.join
-
-    html = <<-HTML
-    <div class="alert alert-danger" id="error_explanation">      
-      #{messages}
-    </div>
-    HTML
-
-    html.html_safe
+    error_messages(resource)
   end
 end
