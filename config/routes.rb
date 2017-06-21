5479aa049f9c1b391dd406af965d2d955044b4cd
Evg <evg.tray@gmail.com>
2017-06-21 16:58:04 +0300

    Remove deletion accounts, #35

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, skip: :registrations
+  devise_scope :user do
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      as: :user_registration do
+        get :cancel
+      end
+  end
 
   authenticate :user, ->(user) { user.is_admin? } do
     mount Sidekiq::Web => '/sidekiq'
